948b47f2b814a93d0b552f6fbe06ea6b172bd0b3
Francisco Romero <francisco.romero@cesurformacion.com>
2023-10-25 12:14:40 +0200

    Modificaciones del footer

--- static/js/app.js
+++ static/js/app.js
@@ -4,6 +4,7 @@ const enlace = document.querySelector("a#enlace_megamenu");
 const megamenu = document.querySelector("nav.megamenu");
 const offcanvas = document.querySelector("nav.offcanvas");
 const enlace_menu_usuario = document.querySelector("a#menu_usuario");
+const loader = document.querySelector("#loader");
 
 /*
 enlace.addEventListener("click", function(){
@@ -15,5 +16,10 @@ enlace.addEventListener("click", () => megamenu.classList.toggle("reveal") );
 
 enlace_menu_usuario.addEventListener("click", function(){
     offcanvas.classList.toggle("reveal");
+})
+
+window.addEventListener("load",()=>{
+    console.log("Página cargada!");
+    loader.classList.add("finished");
 })
 
